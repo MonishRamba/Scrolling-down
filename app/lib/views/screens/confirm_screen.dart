e92933e3f5d73d159e2df248ff339b486777bf6c
TeamCuatro <129485613+TeamCuatro@users.noreply.github.com>
2023-11-04 11:38:08 +0530

    app upload v1

---
+++ app/lib/views/screens/confirm_screen.dart
@@ -0,0 +1,128 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
+
+import '../../controllers/upload_video_controller.dart';
+import '../widgets/text_input_field.dart';
+
+class ConfirmScreen extends StatefulWidget {
+  final File videoFile;
+  final String videoPath;
+  const ConfirmScreen({
+    Key? key,
+    required this.videoFile,
+    required this.videoPath,
+  }) : super(key: key);
+
+  @override
+  State<ConfirmScreen> createState() => _ConfirmScreenState();
+}
+
+class _ConfirmScreenState extends State<ConfirmScreen> {
+  late VideoPlayerController controller;
+  TextEditingController _songController = TextEditingController();
+  TextEditingController _captionController = TextEditingController();
+  TextEditingController _linkController = TextEditingController();
+
+  UploadVideoController uploadVideoController =
+      Get.put(UploadVideoController());
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      controller = VideoPlayerController.file(widget.videoFile);
+    });
+    controller.initialize();
+    controller.play();
+    controller.setVolume(1);
+    controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.6,
+              child: VideoPlayer(controller),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    width: MediaQuery.of(context).size.width - 20,
+                    child: TextInputField(
+                      controller: _songController,
+                      labelText: 'Title',
+                      icon: Icons.title,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    width: MediaQuery.of(context).size.width - 20,
+                    child: TextInputField(
+                      controller: _captionController,
+                      labelText: 'Description',
+                      icon: Icons.subject,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    width: MediaQuery.of(context).size.width - 20,
+                    child: TextInputField(
+                      controller: _linkController,
+                      labelText: 'Link to project',
+                      icon: Icons.link,
+                    ),
+                  ),
+                  // const SizedBox(
+                  //   height: 10,
+                  // ),
+                  ElevatedButton(
+                      onPressed: () => uploadVideoController.uploadVideo(
+                          _songController.text,
+                          _captionController.text,
+                          _linkController.text,
+                          widget.videoPath),
+                      child: const Text(
+                        'Share!',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
